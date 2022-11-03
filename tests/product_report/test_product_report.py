0a791966312c80b62a0f7eadb2747223d09a3405
bruh-git <brunac.cardososilva@gmail.com>
2022-11-03 13:43:11 -0300

    test: req 02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Arroz",
+        "Arroz do Vale",
+        "2020-01-01",
+        "2020-12-31",
+        "123456789",
+        "em local seco",
+    )
+    assert (
+        product.__repr__()
+        == "O produto Arroz"
+        " fabricado em 2020-01-01"
+        " por Arroz do Vale com validade"
+        " até 2020-12-31"
+        " precisa ser armazenado em local seco."
+    )
